675b9024695121b04a47ad625fa25ffec35f18d1
JeffreyKragten <35327042+JeffreyKragten@users.noreply.github.com>
2018-04-01 18:48:04 +0200

    HealthBar moet alleen nog een betere animatie

---
+++ src/main/java/nl/han/Bullet.java
@@ -0,0 +1,51 @@
+package nl.han.ica.PlatoBrawl;
+import java.util.List;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Exceptions.TileNotFoundException;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+
+public class Bullet extends AnimatedSpriteObject implements ICollidableWithGameObjects {
+
+    private final PlatoBrawl world;
+	
+	
+	public Bullet (PlatoBrawl world, float x, int f) {
+        super(new Sprite("src/main/java/nl/han/ica/PlatoBrawl/media/sprites/Bullet.png"), 2);
+		this.world = world;
+		setxSpeed(x);
+		setCurrentFrameIndex(f);
+	}
+
+	
+	@Override
+	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+
+        for (GameObject go : collidedGameObjects) {
+            if (go instanceof Swordfish) {
+            	try {
+                    world.deleteGameObject(this);
+                    ((Swordfish) go).swordfishHit();
+                } catch (TileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	}
+
+
+	@Override
+	public void update() {
+		if (getX() >= world.getWidth()) {
+            world.deleteGameObject(this);
+        }
+		if (getX() <= 0) {
+            world.deleteGameObject(this);
+        }
+		
+	}
+	
+	
+}
